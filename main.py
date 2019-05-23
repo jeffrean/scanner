f370c728306d6982a5cea568fe973c05e2022021
Jeffrey Nagel <jeffrean@usc.edu>
2019-05-22 18:44:46 -0700

    yo

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ video = cv2.VideoCapture(0)
 
 for step in range(0,NUM_STEPS):
 	ret, frame = video.read()
+	cv2.imshow('Video', frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
-video.release_capture()
+video.release()
 cv2.destroyAllWindows()
