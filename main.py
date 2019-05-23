df75b2ae7b065c160463c929af85692abb3a9b09
Jeffrey Nagel <jeffrean@usc.edu>
2019-05-22 19:12:23 -0700

    show laser line

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ video = cv2.VideoCapture(0)
 #for step in range(0,NUM_STEPS):
 while True:
 	ret, frame = video.read()
-	thresh = laser_threshold_image(frame)
+	thresh = scanner.laser_threshold_image(frame)
 	cv2.imshow('Video', thresh)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
